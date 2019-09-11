ea37f1661f9b104c27d25dd7f4fcecf97e7239e7
Oliver <olla@html24.net>
2019-09-11 17:38:16 +0200

    Adds asynchronous action creator via middleware (Redux Thunk)

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    
+    dispatch({ type: 'FETCH_POSTS', payload: response})
+};
---
+++ src/components/PostList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import {fetchPosts} from '../actions';
+
+class PostList extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return (
+        <h1>
+            Post List
+        </h1>
+        )
+    }
+}
+
+export default connect(null, {fetchPosts})(PostList);
