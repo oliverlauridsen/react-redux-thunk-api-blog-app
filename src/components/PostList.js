1c54e14b4e28587580846d89f3ca7741cef4c749
Oliver <olla@html24.net>
2019-09-13 20:09:53 +0200

    Adds several reducers, connects the components to Redux and passes the state of the store to the components. Also adds another get request to the json API, getting the users

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import {fetchPosts} from '../actions';
-
+import UserHeader from './UserHeader';
 class PostList extends Component {
     componentDidMount() {
         this.props.fetchPosts();
     }
 
+    renderList() {
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2> {post.title} </h2>
+                            <p> {post.body} </p>
+                        </div>
+                        <UserHeader userId={post.userId} />
+                    </div>
+                </div>
+            )
+        });
+    }
+
     render() {
         return (
-        <h1>
-            Post List
-        </h1>
+            <div className="ui relaxed divided list"> 
+                {this.renderList()};
+            </div>
         )
     }
 }
 
-export default connect(null, {fetchPosts})(PostList);
+const mapStateToProps = state => {
+    return { posts: state.posts};
+};
+
+export default connect(mapStateToProps, {fetchPosts})(PostList);
